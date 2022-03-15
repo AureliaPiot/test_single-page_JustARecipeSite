80c3841b9786a4e9589d8a8acd9b0a303ebfcb09
AureliaPiot <aurelia.piot@hotmail.com>
2022-03-15 22:17:02 +0100

    first commit

---
+++ JS/index.js
@@ -0,0 +1,51 @@
+const BaseURL = "http://localhost/dev/perso/projet/API_cuisine/theMealDB/Front_V2/";
+console.log(window.location.pathname.split("/"));
+
+window.onload = function(){
+    const path = window.location.pathname.split("/");
+    switch(path[7])
+    {
+        case "":{
+            loadPage("home");
+            break;
+        }
+        case "about":{
+            loadPage("about");
+            break;
+        }        
+        default:{
+            loadPage("404");
+            break;
+        }
+    }//fin switch
+    console.log(document.querySelectorAll(".nav__item"));
+    document.querySelectorAll(".nav__item").forEach((item)=>
+
+    item.addEventListener("click", function(){
+
+        const path = item.getAttribute('value');
+        loadPage(path);
+        if(path == "home")
+        {
+            window.history.pushState("","",BaseURL);
+            return;
+        }
+        window.history.pushState("","",BaseURL+path);
+    })
+    
+    )
+    function loadPage($path){
+
+        if($path == "")return;
+        const container = document.getElementById("container");
+        const request = new XMLHttpRequest();
+        request.open("get","pages/"+$path+".html");
+        request.send();
+        request.onload= function(){
+            if(request.status == 200){
+                container.innerHTML = request.responseText;
+                document.title=$path
+            }
+        }
+    }
+}
